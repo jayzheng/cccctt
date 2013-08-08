714dfd177039a46d155426f579a5a8fcc269564a
Jay Zheng <jayzheng07@gmail.com>
2013-08-07 22:08:43 -0400

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cccctt::Application.config.secret_key_base = '2643dcb214ed23a2b0569e5c7d4904c57690a098cd1744b2fc45b8ce41bd988bee1506582acc8991e6a49923256145c64b58b512788d9bb052b7cf49acdaae90'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cccctt::Application.config.session_store :cookie_store, key: '_cccctt_session'
